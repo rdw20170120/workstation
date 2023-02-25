01ee157bf09d3b22135f7867039a7c36d120b003
Rob Williams <rob@refactory.biz>
2023-02-25 13:41:20 -0500

    Remove obsolete content from `waiting`

--- BriteOnyx/bin/lib/alias.bash
+++ BriteOnyx/bin/lib/alias.bash
@@ -6,12 +6,12 @@
 ###############################################################################
 # Common Bash alias definitions
 
-# TODO: Consider augmenting the other forms of 'grep' too
 grep_options='--color=auto'
 grep_options+=' --exclude-dir=.anaconda'
 grep_options+=' --exclude-dir=.git'
 grep_options+=' --exclude-dir=.pytest_cache'
 grep_options+=' --exclude-dir=coverage'
+# TODO: Consider augmenting the other forms of 'grep' too
 # TODO: It appears that '--exclude-from' is not supported on macOS Mojave 10.14.6
 # TODO: Perhaps I should consider installing a compatible `grep`
 # grep_options+=' --exclude-from="${BO_Project}/.grep-exclude-from"'
@@ -19,12 +19,14 @@ grep_options+=' --exclude-dir=coverage'
 grep_options+=' --exclude="*.pyc"'
 grep_options+=' --exclude="*.swp"'
 alias grep="grep ${grep_options}"
+unset grep_options
 
 alias cycle='clear ; test-run && gen-run -vvv && app-run -vvv'
 alias list_sort_by_size='sort -nr --key=5'
 alias logs_reset='rm -fr "${BO_Project}/log" ; mkdir "${BO_Project}/log"'
 alias redeclare='source "${BO_Project}/BriteOnyx/bin/lib/declare.bash"'
-alias todo='grep -FR TODO "${BO_Project}" | grep -Fv .log | grep -Fv .out'
+alias someday='grep -FiR SOMEDAY "${BO_Project}" | grep -Fv .log | grep -Fv .out'
+alias todo='grep -FiR TODO "${BO_Project}" | grep -Fv .log | grep -Fv .out'
 
 ###############################################################################
 # NOTE: Uncomment these lines for debugging, placed where needed
