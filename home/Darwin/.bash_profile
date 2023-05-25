d6f86cae8fa1cfb3b3987b03e53632bd17e92d90
Rob Williams <Rob.Williams@samsclub.com>
2023-05-25 15:47:16 -0400

    Refactor workstation synchronization

--- home/Darwin/.bash_profile
+++ home/Darwin/.bash_profile
@@ -1,8 +1,6 @@
 #!/bin/false
 # NOTE:  This file is intended to be executed as part of starting a Bash shell.
 
-umask u=rwx,g=,o=
-
 [[ -r ~/.bashrc ]] && source ~/.bashrc
 
 : << 'DisabledContent'
--- home/Darwin/alias.bash
+++ home/Darwin/alias.bash
@@ -42,9 +42,5 @@ alias emacs='/Applications/Emacs.app/Contents/MacOS/emacs-nw'
 
 # ssh
 alias connect_to_dev='ssh DevAtAws'
-
-# Add an "alert" alias for long running commands.  Use like so:
-# sleep 10; alert
-# alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 DisabledContent
 
