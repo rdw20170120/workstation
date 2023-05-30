75e321be0c12b350fd0c396743fec7c7318d4c58
Rob Williams <rob@refactory.biz>
2023-05-30 01:55:52 +0000

    Update source generation

--- .BO/capture/generated/src/gen/generate/config.py
+++ .BO/capture/generated/src/gen/generate/config.py
@@ -1,33 +1,13 @@
 #!/usr/bin/env false
-
-
 """TODO: Write
-
-
-
-
-
-
 """
-
-
 # Internal packages (absolute references, distributed with Python)
-
-
 # External packages (absolute references, NOT distributed with Python)
-
-
 # Library modules   (absolute references, NOT packaged, in project)
-
-
 # Project modules   (relative references, NOT packaged, in project)
 
 
 # TODO: CONTENT
 
 """DisabledContent
-
-
-
-
 """
