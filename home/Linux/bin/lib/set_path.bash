906a68c666944537263dc7df31e80bad8c470fd1
Rob Williams <rob@refactory.biz>
2023-06-17 20:09:25 -0400

    Execute `bin/sync_home` to manually merge files

--- home/Linux/bin/lib/set_path.bash
+++ home/Linux/bin/lib/set_path.bash
@@ -1,47 +1,84 @@
 #!/usr/bin/env cat
 # NOTE:  This file is intended to be sourced into a Bash shell.
 ################################################################################
-# Configure paths
+# Configure paths, especially PATH
 
-################################################################################
-# Remember the native and original system PATHs
-[[ -z "${BO_PathOriginal}" ]] && export BO_PathOriginal=${PATH}
-export BO_PathNative=${BO_PathOriginal}
-
-################################################################################
-# NOTE: Order matters!
-# Anaconda should override all
-# (win all collisions)
+# NOTE: This specific ordering of PATH elements is REQUIRED and ESSENTIAL.
+#
+# The Anaconda environment MUST come first
+# in order to override everything else.
+# Anaconda should override all (win all collisions)
 # as the preferred package manager
 # for projects being developed and/or deployed.
-# However, Anaconda is managed
-# using project-specific environments
-# that are incorporated into the PATH
-# as part of the BriteOnyx activation
-# for each project.
-# Therefore, Anaconda is absent here.
-# Then the system package manager comes second,
-# which is
-# APT for Ubuntu (handled under the native system path)
-# and Homebrew (currently) for Apple macOS.
-# Other non-native tools follow
-# such as VMware Fusion (if installed).
-# The native system path is next.
-# The user path is last
-# (so user MUST resolve collisions).
-# Build up the system path from the end,
-# starting with the native path,
-# then moving forward.
-# This allows easier manipulation by (un)commenting entries.
-BO_PathSystem=${BO_PathNative}
-export BO_PathSystem
-export BO_PathUser=${HOME}/bin
-export PATH=${BO_PathSystem}:${BO_PathUser}
+#
+# The system package manager comes second, which is
+# Homebrew (currently) for Apple macOS.
+# APT for Debian and Ubuntu 
+# are available on the system path,
+# so it does not require special treatment.
+#
+# All such separately-installed
+# package managers, development platforms, and individual tools
+# should override any system-installed programs,
+# therefore they are included in `BO_PathTool`
+# at the front of `PATH`.
+#
+# The original system path `BO_PathOriginal` is next.
+# This usually consists of `PATH` as it exists
+# before executing any user-specific shell login scripts.
+# The system PATH element `BO_PathSystem`
+# MUST precede
+# any user PATH element `BO_PathUser`
+# in order to make collisions fail-fast and
+# to defeat simple attempts at redirecting system commands as an attack vector.
+#
+# Similarly,
+# the project PATH element `BO_PathProject`
+# MUST precede
+# the user PATH element `BO_PathUser`
+# in order to make collisions fail-fast
+# in favor of the project over the user.
+# The user path is last as `BO_PathUser`.
+#
+# This arrangement is best for ensuring consistent behavior
+# of the environment, the system, and the project.
+# It puts at-risk only
+# those user-specific commands, tools, and scripts
+# relevant to the currently-deployed environment--
+# where the specific user is best positioned to address them
+# and failures are most likely limited to affecting only
+# the current project and user (as they should).
 
-################################################################################
-# Anaconda (Mambaforge)
-export CONDA_PREFIX=${HOME}/mambaforge
-export BO_PathAnacondaBase=${CONDA_PREFIX}/bin:${CONDA_PREFIX}/condabin
+# Projects should modify PATH mostly by setting `BO_PathProject`
+[[ -z "${BO_PathProject}" ]] && export BO_PathProject=
+
+# Remember variants of the system `PATH`
+[[ -z "${BO_PathNative}" ]] && export BO_PathNative=${PATH}
+[[ -z "${BO_PathOriginal}" ]] && export BO_PathOriginal=${PATH}
+export BO_PathSystem=${BO_PathOriginal}
+
+# Most system-level modifications to `PATH`
+# should happen to `BO_PathToolOther`
+BO_PathTool=${BO_PathAnaconda}
+BO_PathTool+=:${BO_PathHomebrew}
+BO_PathTool+=:${BO_PathToolOther}
+export BO_PathTool
+
+# Only the user should modify `BO_PathUser`
+[[ -z "${BO_PathUser}" ]] && export BO_PathUser=${HOME}/bin
+
+# This assemblage of `PATH`
+# should remain fixed and invariant,
+# except by modifying the elements properly
+# as described above.
+# TODO: However, a project-specific virtual environment
+# should come FIRST on `PATH`
+# before ANY other elements.
+PATH=${BO_PathTool}
+PATH+=:${BO_PathSystem}
+PATH+=:${BO_PathProject}
+PATH+=:${BO_PathUser}
+export PATH
 
 : << 'DisabledContent'
 DisabledContent
