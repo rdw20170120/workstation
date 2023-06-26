e29dfde88ad85124849add279eaf592f0a8637cf
Rob Williams <Rob.Williams@samsclub.com>
2023-06-25 22:25:17 -0400

    Rewrite `bin/sync_workstation` in Python using process that leverages editor, Move bootstrap scripts out of `home` into their own subdirectory, Move `sync_home` back into its proper place outside of `BriteOnyx`

--- home/customer/credential.bash
+++ home/customer/credential.bash
@@ -6,29 +6,9 @@
 ################################################################################
 # PostgreSQL user passwords
 
-# Default to UNKNOWN
-export DB_PassPqaScusAdmin='UNKNOWN'
-export DB_PassPqaScusRegular='UNKNOWN'
-export DB_PassPqaScusUnknown='UNKNOWN'
-export DB_PassProdScusAdmin='UNKNOWN'
-export DB_PassProdScusRegular='UNKNOWN'
-export DB_PassProdScusUnknown='UNKNOWN'
-export DB_PassProdWusAdmin='UNKNOWN'
-export DB_PassProdWusRegular='UNKNOWN'
-export DB_PassProdWusUnknown='UNKNOWN'
-
-# From Arti
-export DB_PassDevAdmin=
-export DB_PassDevRegular=
-export DB_PassDevUnknown=
-export DB_PassQaAdmin=
-export DB_PassQaRegular=
-export DB_PassQaUnknown=
-
-# From Bhavana
-export DB_PassStageAdmin=
-export DB_PassStageRegular=
-export DB_PassStageUnknown=
+export PostgresPassLocalRegular='L0c@lU53r'
+export PostgresPassRemoteOwner='S@msC1ub#Item'
+export PostgresPassRemoteRegular='l39@t0d3v'
 
 ################################################################################
 : << 'DisabledContent'
